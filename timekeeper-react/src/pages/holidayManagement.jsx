66ddb2a2bb8211e2f44608fa8b46348fa53ae067
Lam Martin <thachlam1997@gmail.com>
2020-09-28 15:21:51 +0700

    To lampt

--- timekeeper-react/src/pages/holidayManagement.jsx
+++ timekeeper-react/src/pages/holidayManagement.jsx
@@ -2,17 +2,186 @@ import React, { Component } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 
-class HolidayManagement extends Component {
-    state = {};
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import { Modal, Form, Button, Col, Alert, Row, ButtonGroup } from 'react-bootstrap';
+import Dialog from 'react-bootstrap-dialog'
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import HolidayService from "../service/HolidayService";
+import { log } from "util";
+
+class HolidayManagement extends Component {
+    state = {
+        modalShow: false,
+        modalNew: true,
+        modalTitle: '',
+        modalErr: '',
+        dateStr: '',
+        txtEvent: '',
+        events: [
+            // { title: 'event 1', date: '2020-09-03' },
+            // { title: 'event 2', date: '2020-09-03' }
+        ]
+    };
+    componentDidMount() {
+        HolidayService.list().then((res) => {
+            this.setState({ events: res.data });
+        });
+    }
+    handleDateClick = (arg) => { // bind with an arrow function
+        if (this.state.events.length == 0) {
+            this.setState({ modalNew: true })
+            this.setState({ modalTitle: 'Thêm mới sự kiện' })
+            this.setState({ modalShow: true })
+            this.setState({ dateStr: arg.dateStr })
+            this.setState({ txtEvent: '' })
+        } else {
+            let eventsMap = this.state.events.map((e) => {
+                return e
+            });
+            let isTexted = eventsMap.find(e => e.date == arg.dateStr);
+            if (isTexted) {
+                this.setState({ modalNew: false })
+                this.setState({ modalTitle: 'Cập nhật sự kiện' })
+                this.setState({ modalShow: true })
+                this.setState({ txtEvent: isTexted.title })
+                this.setState({ dateStr: arg.dateStr })
+
+            } else {
+                this.setState({ modalNew: true })
+                this.setState({ modalTitle: 'Thêm mới sự kiện' })
+                this.setState({ modalShow: true })
+                this.setState({ dateStr: arg.dateStr })
+                this.setState({ txtEvent: '' })
+            }
+        }
+        // 
+
+
+    }
+
+    handleChange = (event) => {
+        this.setState({ txtEvent: event.target.value });
+    }
+    closeModal = () => {
+        this.setState({ modalShow: false })
+    }
+    deleteEvent = () => {
+        
+        let eMap = this.state.events.map((e) => {
+            return e
+        });
+        let eIndex = eMap.findIndex(e => e.date == this.state.dateStr);
+        eMap.splice(eIndex, 1);
+        this.setState({ events: eMap })
+        this.closeModal();
+        HolidayService.delete(this.state.dateStr).then((res) => {
+            console.log(res);
+        });
+
+    }
+    saveEvent = () => {
+        if (this.state.txtEvent == '') {
+            this.setState({ modalErr: 'Không được để trống' })
+        } else {
+            this.setState({ modalErr: '' })
+            if (this.state.modalNew) {
+                if (this.state.events.length == 0) {
+                    let eTempt = [];
+                    eTempt.push({title: this.state.txtEvent, date: this.state.dateStr })
+                    this.setState({ events: eTempt})
+                    this.closeModal();
+                    HolidayService.put(`{"title": "${this.state.txtEvent}", "date": "${this.state.dateStr}" }`).then((res) => {
+                        console.log(res);
+                    });
+                } else {
+                    let eMap = this.state.events.map((e) => {
+                        return e
+                    })
+                    eMap.push({ title: this.state.txtEvent, date: this.state.dateStr });
+                    this.setState({ events: eMap })
+                    //create ar
+                    this.closeModal();
+                    HolidayService.put(`{"title": "${this.state.txtEvent}", "date": "${this.state.dateStr}" }`).then((res) => {
+                        console.log(res);
+                    });
+                }
+
+            } else {
+
+                let eMap = this.state.events.map((e) => {
+                    return e
+                });
+                let eIndex = eMap.findIndex(e => e.date == this.state.dateStr);
+                eMap[eIndex].title = this.state.txtEvent;
+                this.setState({ events: eMap });
+                this.closeModal();
+                HolidayService.update(`{"title": "${this.state.txtEvent}", "date": "${this.state.dateStr}" }`).then((res) => {
+                    console.log(res);
+                });
+            }
+
+        }
+
+
+
+    }
     render() {
         return (
-            <div class="content bg-white">
-                <div class="container-fluid">
+            <div className="content bg-white">
+                <div className="container-fluid">
                     <FullCalendar
-                        plugins={[dayGridPlugin]}
+                        plugins={[dayGridPlugin, interactionPlugin]}
                         initialView="dayGridMonth"
+                        dateClick={this.handleDateClick}
+                        events={this.state.events}
                     />
+
+                </div>
+
+                <div>
+                    <Modal show={this.state.modalShow}
+                        onHide={this.closeModal}
+                        keyboard={false}
+                        animation={false}>
+
+                        <Modal.Header closeButton>
+                            <Modal.Title>{this.state.modalTitle}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group >
+                                    <Form.Label  >Tên sự kiện</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="txtEvent"
+                                        value={this.state.txtEvent}
+                                        onChange={this.handleChange}
+
+                                    />
+                                    <Form.Label className="text-danger">{this.state.modalErr}</Form.Label>
+                                </Form.Group>
+                            </Form>
+
+                        </Modal.Body>
+                        <Modal.Footer>
+
+                            <Button variant="secondary" onClick={this.closeModal}>
+                                Đóng
+                            </Button>
+                            <ButtonGroup>
+                                <Button variant="primary mr-2" onClick={this.saveEvent}>
+                                    Lưu
+                            </Button>
+                                <Button variant="danger" onClick={this.deleteEvent}>
+                                    Xóa
+                            </Button>
+                            </ButtonGroup>
+
+                        </Modal.Footer>
+                    </Modal>
                 </div>
             </div>
         );
--- timekeeper-react/src/pages/timeKeepingManagement.jsx
+++ timekeeper-react/src/pages/timeKeepingManagement.jsx
@@ -1,166 +1,194 @@
 import React, { Component } from 'react';
+import TimeKeeper from './timeKeeper';
+import TimeKeepingService from '../service/TimeKeepingService';
+
 
 class TimeKeepingManagement extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div class="content bg-white">
-                <div class="container-fluid">
-                    <div class="row">
-                        <ControlBox/>
-                        <ResultTable/>
+    constructor(props) {
+        super(props);
+        this.state = {
+            timekeepers: [],
+            timekeeper: {},
+            modalShow: false,
+            modalTitle: '',
+            modalNew: '',
+            selectDay: '',
+            selectMonth: '',
+            selectYear: '',
+            txtDepartment: ''
+        }
+    }
+    
+    loadData = () => {
+        let page = 1;
+        let numberOfResult = 50;
+        let date = '2020-08-25';
+        TimeKeepingService.list(page, numberOfResult, date).then(res => {
+            console.log(res.data.data);
+            this.setState({ timekeepers: res.data.data.data });
+        })
+    }
+    componentDidMount() {
+        this.loadData();
+    }
+
+    renderTime = (time) => {
+        let e = [];
+        time.map((t,i) =>{
+            if (i % 2 ==0) {
+                e.push(<a className="d-inline text-primary mr-2 cursor" >{t}</a>);
+            } else {
+                e.push(<a className="d-inline text-danger mr-2 cursor">{t}</a>);
+            }  
+        })
+        
+        return e;
+        
+    }
+
+
+    render() {
+        return (
+            <div className="content bg-white">
+                <div className="container-fluid">
+                    <div className="row">
+                    <div className="filter-table col-12">
+                <div className="row border-bottom border-info pb-3">
+                    <div className="col-2">
+                        <label className="text-dark h4 mt-0">Chấm công hằng ngày</label>
+                    </div>
+                    <div className="col-5">
+                        <div className="form-row">
+                            <div className="col-auto">
+                                <select className="form-control" id="exampleFormControlSelect1">
+                                    <option>01</option>
+                                    <option>02</option>
+
+                                </select>
+                            </div>
+                            <div className="col-auto">
+                                <select className="form-control" id="exampleFormControlSelect1">
+                                    <option>Tháng 1</option>
+                                    <option>Tháng 2</option>
+                                    <option>Tháng 3</option>
+
+                                </select>
+                            </div>
+                            <div className="col-auto">
+                                <select className="form-control" id="exampleFormControlSelect1">
+                                    <option>2020</option>
+                                    <option>2021</option>
+                                    <option>2022</option>
+
+                                </select>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+
+                </div>
+                <div className="row  my-4">
+                    <div className="col-auto">
+                        <label >Phòng ban</label>
+                    </div>
+                    <div className="col-3">
+                        <div className="form-group">
+                            <input className="form-control" type="text" name="inlineRadioOptions" />
+                        </div>
+                    </div>
+
+                    <div className="col align-self-end">
+
+                        <button className="btn btn-success float-right mt-0">Xuất file</button>
+                        <button className="btn btn-warning float-right mt-0  mx-2">Nạp file</button>
+                    </div>
+
+                </div>
+                <div className="row justify-content-center">
+                    <div className="col-auto">
+                        <div className="form-check form-check-inline text-info mr-5">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions"
+                                value="option1" />
+                            <label className="form-check-label ml-0 pl-1" >Tất cả</label>
+                        </div>
+
+                        <div className="form-check form-check-inline text-danger">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions"
+                                value="option3" />
+                            <label className="form-check-label ml-0 pl-1" >Chưa hợp lệ</label>
+                        </div>
+
                     </div>
                 </div>
             </div>
-         );
-    }
-}
 
-class ControlBox extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div class="filter-table col-12">
-                            <div class="row border-bottom border-info pb-3">
-                                <div class="col-2">
-                                    <label class="text-dark h4 mt-0">Chấm công hằng ngày</label>
-                                </div>
-                                <div class="col-5">
-                                    <div class="form-row">
-                                        <div class="col-auto">
-                                            <select class="form-control" id="exampleFormControlSelect1">
-                                                <option>01</option>
-                                                <option>02</option>
+      
+            <div className="main-table col-12  p-3 mt-3">
+                <div className="">
+                    <table className="table table-hover table-bordered">
+                        <thead className="thead-light">
+                            <th scope="col">#</th>
+                            <th>Mã nhân viên</th>
+                            <th>Tên nhân viên</th>
+                            <th>Phòng ban</th>
+                            <th>Vị trí</th>
+                            <th>Ca làm việc</th>
 
-                                            </select>
-                                        </div>
-                                        <div class="col-auto">
-                                            <select class="form-control" id="exampleFormControlSelect1">
-                                                <option>Tháng 1</option>
-                                                <option>Tháng 2</option>
-                                                <option>Tháng 3</option>
+                            <th colspan="2">Thời gian</th>
 
-                                            </select>
-                                        </div>
-                                        <div class="col-auto">
-                                            <select class="form-control" id="exampleFormControlSelect1">
-                                                <option>2020</option>
-                                                <option>2021</option>
-                                                <option>2022</option>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.timekeepers.map( (timekeeper, idx) => {
+                                    return (
 
-                                            </select>
-                                        </div>
-                                    </div>
-
-
-                                </div>
-
-
-                            </div>
-                            <div class="row  my-4">
-                                <div class="col-auto">
-                                        <label for="">Phòng ban</label>
-                                </div>
-                                <div class="col-3">
-                                    <div class="form-group">
-                                        <input class="form-control" type="text" name="inlineRadioOptions"/>
-                                    </div>
-                                </div>
-                                
-                                <div class="col align-self-end">
-                                    
-                                    <button class="btn btn-success float-right mt-0">Xuất file</button>
-                                    <button class="btn btn-warning float-right mt-0  mx-2">Nạp file</button>
-                                </div>
-
-                            </div>
-                            <div class="row justify-content-center">
-                                    <div class="col-auto">
-                                            <div class="form-check form-check-inline text-info mr-5">
-                                                <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                                    value="option1"/>
-                                                <label class="form-check-label ml-0 pl-1" for="inlineRadio1">Tất cả</label>
-                                            </div>
-        
-                                            <div class="form-check form-check-inline text-danger">
-                                                <input class="form-check-input" type="radio" name="inlineRadioOptions"
-                                                    value="option3"/>
-                                                <label class="form-check-label ml-0 pl-1" for="inlineRadio3">Chưa hợp lệ</label>
-                                            </div>
-        
-                                        </div>
-                            </div>
-                        </div>
-                        
-         );
-    }
-}
-
-class ResultTable extends Component {
-    state = {  }
-
-    renderTableRows(num) {
-        const elementList = [];
-        for (let i = 0; i < num; i++) {
-            elementList.push(<TableRow/>);
-        }
-        console.log(elementList);
-        return elementList;
-    }
-
-    render() { 
-        return ( 
-            <div class="main-table col-12  p-3 mt-3">
-                            <div class="">
-                                <table class="table table-hover table-bordered">
-                                    <thead class="thead-light">
-                                        <th scope="col">#</th>
-                                        <th>Mã nhân viên</th>
-                                        <th>Tên nhân viên</th>
-                                        <th>Phòng ban</th>
-                                        <th>Vị trí</th>
-                                        <th>Ca làm việc</th>
-
-                                        <th colspan="2">Thời gian</th>
-
-                                    </thead>
-                                    <tbody>
-                                        {this.renderTableRows(15)}
-                                    </tbody>
-                                </table>
-
-                            </div>
-                        </div>
-                    
-         );
-    }
-}
-
-class TableRow extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <tr>
-                                            <td>1</td>
-                                            <td>NHV0001</td>
-                                            <td>Lê Tùng Dương</td>
-                                            <td>D23 Dongjin</td>
-                                            <td>Công nhân </td>
+                                        <tr key={timekeeper.employeeId}>
+                                            <td>{idx + 1}</td>
+                                            <td>{timekeeper.employeeCode}</td>
+                                            <td>{timekeeper.fullName}</td>
+                                            <td>{timekeeper.department}</td>
+                                            <td>{timekeeper.position}</td>
+                                            <td>{timekeeper.shift}</td>
                                             <td>
-                                                CA1
+                                                {this.renderTime(timekeeper.time)}
                                             </td>
                                             <td>
-                                                <a class="d-inline text-primary" href="">6:00</a>
-                                                <a class="d-inline text-danger" href="">6:00</a>
-                                            </td>
-                                            <td>
-                                                <a class="text-info">
-                                                    <i class="fa fa-plus"></i>
-                                                </a>
+                                                <span onClick={() => { this.showModal(timekeeper.employeeId) }} className="cussor"> <i className="fa fa-edit text-success"></i> </span>
+                                                <a onClick={(e) => { e.preventDefault(); this.showConfirm(timekeeper.employeeId); }} className="cussor"> <i className="fa fa-trash text-danger"></i> </a> 
                                             </td>
                                         </tr>
-         );
+                                    );
+                                })
+                            }
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+
+        
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default TimeKeepingManagement;
